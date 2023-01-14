bb1609eb5860971e50ec52f819fe8c599c1e6426
MySelf-whs <1113483142@qq.com>
2023-01-14 22:37:01 +0800

    first C+

--- src/main/java/com/Liyue/git/Hutao.java
+++ src/main/java/com/Liyue/git/Hutao.java
@@ -3,5 +3,6 @@ package com.Liyue.git;
 public class Hutao {
     public static void main(String[] args) {
         System.out.println("吃饱喝饱，一路走好！");
+        System.out.println("我要护膜！！！");
     }
 }
